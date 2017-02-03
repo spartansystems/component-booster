c433eb14a8466841694fcd9616f8e858e50ad80f
Andrew Frank <afrank@spartansystems.co>
2017-02-03 14:19:18 -0500

    updated story file for this component and the SectionFullWidth one.

--- components/SectionContainer/stories.js
+++ components/SectionContainer/stories.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { storiesOf } from '@kadira/storybook'
-import { WithNotes } from '@kadira/storybook-addon-notes'
+// import { WithNotes } from '@kadira/storybook-addon-notes'
 import { withKnobs, text, select } from '@kadira/storybook-addon-knobs'
 
 import SectionContainer from './component'
 
-const notes = 'This story demonstrates the props that can be passed to SectionContainer.'
+// const notes = 'This story demonstrates the props that can be passed to SectionContainer.'
 
 const props = {
   customClass: 'customClass',
